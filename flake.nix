69ef0fc32bc705b5522dfd03cb3bbeac76714b22
Nathan Henrie <nate@n8henrie.com>
2023-08-15 07:18:40 -0600

    Add cpp output

--- flake.nix
+++ flake.nix
@@ -7,6 +7,10 @@
         path = ./trivial;
         description = "Trivial with multi-system outputs";
       };
+      cpp = {
+        path = ./cpp;
+        description = "Basic example compiling a cpp program";
+      };
     };
   };
 }
