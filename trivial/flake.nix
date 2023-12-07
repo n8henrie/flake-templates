179290b760922820bd81ce45031e7d0b5a427a55
Nathan Henrie <nate@n8henrie.com>
2023-12-07 10:18:13 -0700

    Update to 23.11

--- trivial/flake.nix
+++ trivial/flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.05";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
 
   outputs = {
     self,
