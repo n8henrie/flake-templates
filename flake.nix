fb0ae31f58610b414aab23fabfd0fbbb9931e5e1
Nathan Henrie <nate@n8henrie.com>
2023-12-16 11:35:08 -0700

    Reorganize templates using convenience function
    Also adds new jupyterlab template

--- flake.nix
+++ flake.nix
@@ -1,32 +1,22 @@
 {
   description = "n8henrie's flake templates";
 
-  outputs = {...}: {
-    templates = {
-      aarch64-darwin-trivial = {
-        path = ./aarch64-darwin-trivial;
-        description = "Very basic setup for aarch64-darwin";
-      };
-      convert-shell-script = {
-        path = ./convert-shell-script;
-        description = "Nixify an existing shell script";
-      }:
-      cpp = {
-        path = ./cpp;
-        description = "Basic example compiling a cpp program";
-      };
-      rust = {
-        path = ./rust;
-        description = "Basic rust template";
-      };
-      selenium-rs = {
-        path = ./selenium-rs;
-        description = "Example using thirtyfour for selenium in rust";
-      };
-      trivial = {
-        path = ./trivial;
-        description = "Trivial with multi-system outputs";
-      };
+  outputs = {nixpkgs, ...}: let
+    inherit (nixpkgs) lib;
+    getTemplate = name: {
+      path = ./${name};
+      inherit (import ./${name}/flake.nix) description;
     };
+    templateBuilder = names: lib.genAttrs names getTemplate;
+  in {
+    templates = templateBuilder [
+      "aarch64-darwin-trivial"
+      "convert-shell-script"
+      "cpp"
+      "jupyterlab"
+      "rust"
+      "selenium-rs"
+      "trivial"
+    ];
   };
 }
---
+++ jupyterlab/flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Basic setup for data sciencey stuff with jupyter / python";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    name = "jupyter-example";
+    inherit (nixpkgs) lib;
+    systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
+    systemClosure = attrs:
+      builtins.foldl' (acc: system:
+        lib.recursiveUpdate acc (attrs system)) {}
+      systems;
+  in
+    systemClosure (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [self.overlays.default];
+        };
+      in {
+        overlays = {
+          default = self.overlays.${name};
+          ${name} = _: prev: {
+            # inherit doesn't work with dynamic attributes
+            ${name} = (self.packages.${prev.system}).${name};
+          };
+        };
+        packages.${system} = let
+          jupyter-black = let
+            pname = "jupyter-black";
+            version = "0.3.4";
+          in
+            pkgs.python311Packages.buildPythonPackage {
+              inherit pname version;
+              format = "pyproject";
+              propagatedBuildInputs = with pkgs.python311Packages; [
+                black
+                setuptools-scm
+              ];
+              src = pkgs.fetchPypi {
+                inherit pname version;
+                hash = "sha256-KjjzPUwyHrdo9CYQNjWsm4C0DJ5CqgYHKnKePK3cpMM=";
+              };
+            };
+        in {
+          default = pkgs.python311.withPackages (ps:
+            with ps; [
+              jupyter-black
+              jupyterlab
+              matplotlib
+              pandas
+              polars
+              pyarrow
+            ]);
+        };
+
+        apps.${system}.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/jupyter-lab";
+        };
+      }
+    );
+}
