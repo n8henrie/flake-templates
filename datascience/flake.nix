4ce7dcc61e78c9f2d96c4e383b3fa4c2246a5b2d
Nathan Henrie <nate@n8henrie.com>
2025-10-08 15:14:51 -0600

    Initial commit

---
+++ datascience/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Basic setup for data sciencey stuff";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-darwin"
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      eachSystem =
+        with nixpkgs.lib;
+        f: foldAttrs mergeAttrs { } (map (s: mapAttrs (_: v: { ${s} = v; }) (f s)) systems);
+    in
+    eachSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs
+            (python3.withPackages (
+              ps: with ps; [
+                altair
+                marimo
+                polars
+                ruff
+                sqlalchemy
+              ]
+            ))
+          ];
+        };
+      }
+    );
+}
